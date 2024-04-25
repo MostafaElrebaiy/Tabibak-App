d76bb6ef1e23236c8892232574f19dfa43c94aac
MostafaElrebaiy <darshmostafa690@gmail.com>
2024-04-25 13:50:31 +0200

    add flavor and fastlane -delete main -create main_development -create main_production -add fastlane -run first fastlane 1.0.1

---
+++ lib/main_development.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tabibk/core/helper/simple_bloc_observer.dart';
+
+import 'core/di/dependancy_injection.dart';
+import 'core/routing/app_router.dart';
+import 'tabibk_app.dart';
+
+void main() async{
+  setupGetIt();
+  //fix hide text 
+  await ScreenUtil.ensureScreenSize();
+  WidgetsFlutterBinding.ensureInitialized();
+   Bloc.observer = SimpleBlocObserver();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(TabibkApp(
+    appRouter: AppRouter(),
+  ));
+}
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tabibk/core/helper/simple_bloc_observer.dart';
 
 import 'core/di/dependancy_injection.dart';
 import 'core/routing/app_router.dart';
 import 'tabibk_app.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  //fix hide text
+  await ScreenUtil.ensureScreenSize();
   WidgetsFlutterBinding.ensureInitialized();
-   Bloc.observer = SimpleBlocObserver();
+  Bloc.observer = SimpleBlocObserver();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(TabibkApp(
