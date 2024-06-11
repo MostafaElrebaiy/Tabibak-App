b84cb5e7ee3eb1272becd9c43d98ed8623c57370
Abdallh Mostafa elRabiey <abdallhelrabiey255@gmail.com>
2024-06-11 17:39:49 +0300

    add bloc consumer

--- lib/features/blood_bank/ui/widgets/build_select_blood_type.dart
+++ lib/features/blood_bank/ui/widgets/build_select_blood_type.dart
@@ -6,9 +6,12 @@ import 'package:tabibk/core/di/dependancy_injection.dart';
 import 'package:tabibk/core/helper/app_assets.dart';
 import 'package:tabibk/core/helper/app_localization.dart';
 import 'package:tabibk/core/helper/app_string.dart';
+import 'package:tabibk/core/helper/extensions.dart';
 import 'package:tabibk/core/helper/spacing.dart';
+import 'package:tabibk/core/routing/routes.dart';
 import 'package:tabibk/core/theme/app_colors.dart';
 import 'package:tabibk/core/theme/styles.dart';
+import 'package:tabibk/core/utilities/show_failure_dialog.dart';
 import 'package:tabibk/core/widgets/custom_widget/app_text_button.dart';
 import 'package:tabibk/features/blood_bank/logic/cubit/blood_bank_cubit.dart';
 import 'package:tabibk/features/blood_bank/logic/cubit/blood_bank_state.dart';
@@ -24,114 +27,124 @@ Future<dynamic> selectBloodType(
     builder: (BuildContext context) {
       return BlocProvider(
         create: (context) => BloodBankCubit(getIt()),
-        child: BlocBuilder<BloodBankCubit, BloodBankState>(
-          builder: (context, state) {
-            return StatefulBuilder(
-              builder: (context, setState) => SizedBox(
-                width: double.infinity,
-                height: 700.h,
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50),
-                        ),
-                      ),
-                      width: double.infinity,
-                      height: 700.h,
-                      child: Column(
-                        children: [
-                          verticalSpace(25.h),
-                          Text(AppString.sort.tr(context),
-                              style: AppStyle.font20GrayBold),
-                          Expanded(
-                            child: ListView.separated(
-                                separatorBuilder: (context, index) =>
-                                    const Divider(
-                                      color: AppColor.gray,
-                                      thickness: .2,
-                                      height: 10,
-                                    ),
-                                itemCount: bloodGroup.length,
-                                itemBuilder: (context, index) {
-                                  context.read<BloodBankCubit>().bloodType =
-                                      bloodGroup[index];
-                                  return RadioListTile(
-                                    title: Text(bloodGroup[index],
-                                        style: AppStyle.f20Bold),
-                                    controlAffinity:
-                                        ListTileControlAffinity.trailing,
-                                    activeColor: AppColor.mainBlue,
-                                    selected:
-                                        currentBloodGroup == bloodGroup[index]
-                                            ? true
-                                            : false,
-                                    value: bloodGroup[index],
-                                    groupValue: currentBloodGroup,
-                                    onChanged: (type) {
-                                      setState(() {
-                                        currentBloodGroup = type.toString();
-                                      });
-                                    },
-                                  );
-                                }),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                width: 1,
-                                color: AppColor.mainBlue,
-                              ),
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            child: AppTextButton(
-                              backgroundColor: AppColor.white,
-                              buttonText: AppString.submit.tr(context),
-                              textStyle: AppStyle.f22mainBlueBold,
-                              onPressed: () {
-                                // context.pop();
-                                if (context.read<BloodBankCubit>().bloodType ==
-                                    null) {
-                                  return;
-                                }
-                                context
-                                    .read<BloodBankCubit>()
-                                    .searchForBloodType(
-                                        bloodType: currentBloodGroup);
-                                // context.pushNamed(Routes.bloodBankSearchScreen);
-                              },
-                              buttonWidth: 220.w,
-                              buttonHeight: 55.h,
-                            ),
-                          ),
-                          verticalSpace(8.h)
-                        ],
+        child: BlocConsumer<BloodBankCubit, BloodBankState>(
+            builder: (context, state) {
+          return StatefulBuilder(
+            builder: (context, setState) => SizedBox(
+              width: double.infinity,
+              height: 700.h,
+              child: Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
                       ),
                     ),
-                    Positioned(
-                      top: -40,
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: AppColor.mainBlue,
-                        child: SvgPicture.asset(
-                          AppAsset.downDialogIcon,
-                          height: 20,
-                          width: 20,
+                    width: double.infinity,
+                    height: 700.h,
+                    child: Column(
+                      children: [
+                        verticalSpace(25.h),
+                        Text(AppString.sort.tr(context),
+                            style: AppStyle.font20GrayBold),
+                        Expanded(
+                          child: ListView.separated(
+                              separatorBuilder: (context, index) =>
+                                  const Divider(
+                                    color: AppColor.gray,
+                                    thickness: .2,
+                                    height: 10,
+                                  ),
+                              itemCount: bloodGroup.length,
+                              itemBuilder: (context, index) {
+                                context.read<BloodBankCubit>().bloodType =
+                                    bloodGroup[index];
+                                return RadioListTile(
+                                  title: Text(bloodGroup[index],
+                                      style: AppStyle.f20Bold),
+                                  controlAffinity:
+                                      ListTileControlAffinity.trailing,
+                                  activeColor: AppColor.mainBlue,
+                                  selected:
+                                      currentBloodGroup == bloodGroup[index]
+                                          ? true
+                                          : false,
+                                  value: bloodGroup[index],
+                                  groupValue: currentBloodGroup,
+                                  onChanged: (type) {
+                                    setState(() {
+                                      currentBloodGroup = type.toString();
+                                    });
+                                  },
+                                );
+                              }),
                         ),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1,
+                              color: AppColor.mainBlue,
+                            ),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: AppTextButton(
+                            backgroundColor: AppColor.white,
+                            buttonText: AppString.submit.tr(context),
+                            textStyle: AppStyle.f22mainBlueBold,
+                            onPressed: () {
+                              // context.pop();
+                              if (context.read<BloodBankCubit>().bloodType ==
+                                  null) {
+                                return;
+                              }
+                              context.read<BloodBankCubit>().searchForBloodType(
+                                  bloodType: currentBloodGroup);
+                              // context.pushNamed(Routes.bloodBankSearchScreen);
+                            },
+                            buttonWidth: 220.w,
+                            buttonHeight: 55.h,
+                          ),
+                        ),
+                        verticalSpace(8.h)
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: -40,
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: AppColor.mainBlue,
+                      child: SvgPicture.asset(
+                        AppAsset.downDialogIcon,
+                        height: 20,
+                        width: 20,
                       ),
                     ),
-
-                  ],
-                ),
+                  ),
+                ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        }, listener: (context, state) {
+          state.when(
+            initial: () => const SizedBox.shrink(),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            success: (bloodBankResponse) {
+              context.pop();
+              context.pushNamed(Routes.bloodBankSearchScreen,
+                  arguments: bloodBankResponse);
+            },
+            error: (error) {
+              context.pop();
+              showFailureDialog(context, error);
+            },
+          );
+        }),
       );
     },
   );
