ea059b078c152bf4d6f4af2da30002a369447942
MostafaElrebaiy <darshmostafa690@gmail.com>
2024-06-17 22:32:43 +0300

    build

---
+++ lib/features/auth/user_details/data/model/user_details_response.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_details_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserDetailsRespons _$UserDetailsResponsFromJson(Map<String, dynamic> json) =>
+    UserDetailsRespons(
+      message: json['message'] as String?,
+      data: json['data'] == null
+          ? null
+          : Data.fromJson(json['data'] as Map<String, dynamic>),
+      code: (json['code'] as num?)?.toInt(),
+    );
+
+Map<String, dynamic> _$UserDetailsResponsToJson(UserDetailsRespons instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+      'data': instance.data,
+      'code': instance.code,
+    };
+
+Data _$DataFromJson(Map<String, dynamic> json) => Data(
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      email: json['email'] as String?,
+      image: json['image'] as String?,
+      createdAt: json['createdAt'] as String?,
+      updatedAt: json['updatedAt'] as String?,
+    );
+
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+      'image': instance.image,
+      'createdAt': instance.createdAt,
+      'updatedAt': instance.updatedAt,
+    };
